173caee31f678e79fb852dd7fc60b6b94057b18f
David Ávila <david.avila.membrives@gmail.com>
2021-09-16 19:14:10 +0200

    Modify script to download DocFX only when is not installed

--- build/buildDoc.ps1
+++ build/buildDoc.ps1
@@ -1,5 +1,11 @@
 # Install docfx
-mkdir -Force Tools
-$toolsDir = Get-Location
-(New-Object Net.WebClient).DownloadFile('https://github.com/dotnet/docfx/releases/download/v2.58.4/docfx.zip', "$toolsDir\Tools\docfx.zip");(new-object -com shell.application).namespace("$toolsDir\Tools").CopyHere((new-object -com shell.application).namespace("$toolsDir\Tools\docfx.zip").Items(),16)
+
+$docfxInstalled = Test-Path -Path .\Tools\docfx.exe
+if(-Not ($docfxInstalled))
+{	
+	mkdir -Force Tools
+	$toolsDir = Get-Location
+	(New-Object Net.WebClient).DownloadFile('https://github.com/dotnet/docfx/releases/download/v2.58.4/docfx.zip', "$toolsDir\Tools\docfx.zip");(new-object -com shell.application).namespace("$toolsDir\Tools").CopyHere((new-object -com shell.application).namespace("$toolsDir\Tools\docfx.zip").Items(),16)
+}
+
 Tools\docfx ..\docfx.json
